183989c973bae55d3dd0a10d6994fcd4e28e49d0
Wellington Mekhoe <w.mekhoe@medicalschemes.com>
2020-04-13 10:07:21 +0200

    - Added tabler assest. - Fixed some css errors.

---
+++ src/TablerAspNetCore.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TablerAspNetCore.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
